2bbbc675d36f7795941e166bd1c51b45e953f692
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-07-20 12:41:40 +0200

    reverse sentence

---
+++ easy-1/5.rb
@@ -0,0 +1,15 @@
+
+def reverse_sentence(text)
+  text = text.split(' ')
+  modified = Array.new
+  i = text.length
+  while i > 0 do
+    modified.push(text.pop)
+    i -= 1
+  end
+  modified.join(' ')
+end
+
+puts reverse_sentence('') == ''
+puts reverse_sentence('Hello World') == 'World Hello'
+puts reverse_sentence('Reverse these words') == 'words these Reverse'
