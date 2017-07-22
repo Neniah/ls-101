f6c2ba71e5836f1ddde3cd9846b2e770f0c53dfb
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-07-22 15:55:55 +0200

    rubocop

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,22 +1,22 @@
-puts ">> Enter one number:"
-number_1 = gets.chomp.to_i
-puts ">> Enter the second number: "
-number_2 = gets.chomp.to_i
-puts "Choose operation: sum ('sm'), subtract('sb'), multiply('m') or division(d):"
+puts 'Enter one number:'
+number1 = gets.chomp.to_i
+puts 'Enter the second number: '
+number2 = gets.chomp.to_i
+puts "Choose operation: sum('sm'), subtract('sb'), multiply('m'), division(d):"
 operation = gets.chomp
 
 case operation
 when 'sm'
-  total = number_1 + number_2
+  total = number1 + number2
   puts "The sum is: #{total}"
 when 'sb'
-  total = number_1 - number_2
+  total = number1 - number2
   puts "The subtract is: #{total}"
 when 'm'
-  total = number_1 * number_2
+  total = number1 * number2
   puts "The multiply is: #{total}"
 when 'd'
-  total = number_1.to_f / number_2.to_f
+  total = number1.to_f / number2.to_f
   puts "The division is: #{total}"
 
 else
