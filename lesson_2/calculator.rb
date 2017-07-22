b944765196f7d7e834ff8743d82ab7e6b0164f9a
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-07-22 14:51:47 +0200

    calculator

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -4,3 +4,21 @@ puts ">> Enter the second number: "
 number_2 = gets.chomp.to_i
 puts "Choose operation: sum ('sm'), subtract('sb'), multiply('m') or division(d):"
 operation = gets.chomp
+
+case operation
+when 'sm'
+  total = number_1 + number_2
+  puts "The sum is: #{total}"
+when 'sb'
+  total = number_1 - number_2
+  puts "The subtract is: #{total}"
+when 'm'
+  total = number_1 * number_2
+  puts "The multiply is: #{total}"
+when 'd'
+  total = number_1.to_f / number_2.to_f
+  puts "The division is: #{total}"
+
+else
+  puts "Sorry, we don't have that option."
+end
