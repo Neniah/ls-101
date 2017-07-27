1032b0cbd0e1105d53078f2f802d1a9cfd64112f
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-07-27 12:08:29 +0200

    rock

--- lesson_3/medium-1/8.rb
+++ lesson_3/medium-1/8.rb
@@ -1,9 +1,18 @@
-answer = 42
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
 
-def mess_with_it(some_number)
-  some_number += 8
+def mess_with_demographics(demo_hash)
+  demo_hash.values.each do |family_member|
+    family_member["age"] += 42
+    family_member["gender"] = "other"
+  end
 end
 
-new_answer = mess_with_it(answer)
+mess_with_demographics(munsters)
 
-p answer - 8
+p munsters
