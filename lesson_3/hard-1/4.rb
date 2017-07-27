a5bd351ef1a2ce1b8650dee132820863d110e725
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-07-27 19:18:37 +0200

    hard 1

---
+++ lesson_3/hard-1/4.rb
@@ -0,0 +1,19 @@
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false if dot_separated_words.size > 4
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false unless !is_an_ip_number?(word)
+  end
+  return true
+end
+
+def is_an_ip_number?(word)
+  word = word.to_i
+  false unless (word <= 0 && word >= 255)
+end
+
+# false condition
+# more than 4 numbers
+
+puts dot_separated_ip_address?("123.98.2.4.267")
