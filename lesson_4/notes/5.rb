464337879d0d433344a21a0dfa6ba5f1c706a9c0
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-07-28 12:34:33 +0200

    hashes

---
+++ lesson_4/notes/5.rb
@@ -0,0 +1,5 @@
+str = 'ghijk'
+arr = ['g', 'h', 'i', 'j', 'k']
+
+puts str[-6]
+puts arr[-6]
---
+++ lesson_4/notes/6.rb
@@ -0,0 +1,3 @@
+hsh = { sky: "blue", grass: "green" }
+puts hsh
+puts hsh.to_a # => [[:sky, "blue"], [:grass, "green"]]
