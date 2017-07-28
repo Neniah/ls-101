4bee8bdc1c510a80cb973e325b0a329337f24188
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-07-28 19:21:04 +0200

    palindrome

---
+++ small-problems/easy-3/8.rb
@@ -0,0 +1,11 @@
+def palindrome?(arr)
+  arr == arr.reverse
+end
+
+
+
+
+puts palindrome?('madam') == true
+puts palindrome?('Madam') == false          # (case matters)
+puts palindrome?("madam i'm adam") == false # (all characters matter)
+puts palindrome?('356653') == true
