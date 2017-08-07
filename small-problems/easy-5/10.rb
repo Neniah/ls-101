dacbcf4036274518876603ce76505173c9ef8060
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-07 18:44:55 +0200

    printing something

---
+++ small-problems/easy-5/10.rb
@@ -0,0 +1,12 @@
+def print_in_box(text)
+  len = text.size
+  puts "+-" + ('-' * len) + '-+'
+  puts "| " + (' ' * len) + ' |'
+  puts "| #{text} |"
+  puts "| " + (' ' * len) + ' |'
+  puts "+-" + ('-' * len) + '-+'
+
+end
+
+print_in_box('To boldly go where no one has gone before.')
+print_in_box('')
