e6f5e15facfe5c2e5333aa99dacf0ad874d8ead8
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-07 16:33:08 +0200

    removing duplicates

---
+++ small-problems/easy-5/9.rb
@@ -0,0 +1,9 @@
+def crunch(text)
+  text = text.squeeze('a-z0-9A-Z')
+end
+
+puts crunch('ddaaiillyy ddoouubbllee') == 'daily double'
+puts crunch('4444abcabccba') == '4abcabcba'
+puts crunch('ggggggggggggggg') == 'g'
+puts crunch('a') == 'a'
+puts crunch('') == ''
