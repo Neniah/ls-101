5e78a6a0aee601ceec9f7f0fe99a008346dda6fc
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-08 11:10:36 +0200

    removing vowels

---
+++ small-problems/easy-6/2.rb
@@ -0,0 +1,12 @@
+def remove_vowels(arr)
+  arr.each do |item|
+    item.gsub!(/[aeiou]/i, '')
+  end
+  arr
+end
+
+
+
+puts remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
+puts remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
+puts remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
