04ed503aabfa44b597bbdb3f5f3e2bf5670ef79e
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-09 12:33:45 +0200

    include

--- small-problems/easy-6/9.rb
+++ small-problems/easy-6/9.rb
@@ -2,6 +2,11 @@ def include?(arr, value)
   !!arr.find_index(value)
 end
 
+def include?(arr, value)
+  arr.each { |element| return true if value == element }
+  false
+end
+
 puts include?([1,2,3,4,5], 3) == true
 puts include?([1,2,3,4,5], 6) == false
 puts include?([], 3) == false
