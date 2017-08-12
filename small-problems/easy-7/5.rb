bcd11b5a7f2ac31f3f8afe24083ce5a6d740030f
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-12 20:06:09 +0200

    alternative upcase and downcase

---
+++ small-problems/easy-7/5.rb
@@ -0,0 +1,17 @@
+def staggered_case(text)
+  result = ''
+  need_upper = true
+  text.chars.each do |char|
+    if need_upper
+      result += char.upcase
+    else
+      result += char.downcase
+    end
+    need_upper = !need_upper
+  end
+  result
+end
+
+puts staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+puts staggered_case('ALL_CAPS') == 'AlL_CaPs'
+puts staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
