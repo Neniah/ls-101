708a3b44b54a014fa126b808b0c43e767266e202
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-12 19:43:52 +0200

    other versions

--- small-problems/easy-7/3.rb
+++ small-problems/easy-7/3.rb
@@ -4,6 +4,17 @@ def word_cap(text)
   text.join(' ')
 end
 
+def word_cap(words)
+  words_array = words.split.map do |word|
+    word.capitalize
+  end
+  words_array.join(' ')
+end
+
+def word_cap(words)
+  words.split.map(&:capitalize).join(' ')
+end
+
 puts word_cap('four score and seven') == 'Four Score And Seven'
 puts word_cap('the javaScript language') == 'The Javascript Language'
 puts word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
