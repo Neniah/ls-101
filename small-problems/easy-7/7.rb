68a12f633cf066cdf2a97a2754390d0425d5d1b7
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-13 18:41:01 +0200

    average

--- small-problems/easy-7/7.rb
+++ small-problems/easy-7/7.rb
@@ -1,16 +1,17 @@
 def show_multiplicative_average(arr)
   len = arr.length
   product = 1.to_f
-  result = arr.each { |number| product * number}
-  average = result / len
+  arr.each { |number| product *= number}
+  average = product / arr.size
   puts format('%.3f', average)
 end
 
-show_multiplicative_average([3, 5])
+
+puts show_multiplicative_average([3, 5])
 #The result is 7.500
 
-show_multiplicative_average([6])
+puts show_multiplicative_average([6])
 #The result is 6.000
 
-show_multiplicative_average([2, 5, 7, 11, 13, 17])
+puts show_multiplicative_average([2, 5, 7, 11, 13, 17])
 #The result is 28361.667
