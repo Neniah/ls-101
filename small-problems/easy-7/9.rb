b8ac0802a313c63987409bb3900e677099d4aa90
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-13 19:45:06 +0200

    sorting products

---
+++ small-problems/easy-7/9.rb
@@ -0,0 +1,11 @@
+def multiply_all_pairs(array1, array2)
+  new_array = []
+  array1.eac do |number|
+    array2.each do |number2|
+      new_array << number * number2
+    end
+  end
+  new_array.sort
+end
+
+puts multiply_all_pairs([2, 4], [4, 3, 1, 2]) == [2, 4, 4, 6, 8, 8, 12, 16]
