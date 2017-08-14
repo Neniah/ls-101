339ed475d6a415a4e41625b352595e5e960b6118
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-14 19:12:29 +0200

    substrings

---
+++ small-problems/easy-8/4.rb
@@ -0,0 +1,26 @@
+def substrings(string)
+  results = []
+  (0...string.size).each do |start_index|
+    this_substring = string[start_index..-1]
+    results.concat(substrings_at_start(this_substring))
+  end
+  results
+end
+
+def substrings_at_start(arr)
+  new_arr = []
+  0.upto(arr.size - 1) do |index|
+    new_arr << arr[0..index]
+  end
+  new_arr
+end
+
+
+
+puts substrings('abcde') == [
+  'a', 'ab', 'abc', 'abcd', 'abcde',
+  'b', 'bc', 'bcd', 'bcde',
+  'c', 'cd', 'cde',
+  'd', 'de',
+  'e'
+]
