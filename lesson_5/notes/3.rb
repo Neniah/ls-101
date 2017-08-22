70edbbe1fcf244afee65f23310044375f7fed642
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-22 20:28:28 +0200

    sorting

---
+++ lesson_5/notes/3.rb
@@ -0,0 +1,5 @@
+def remove_char(s)
+  s[1..-2]
+end
+
+puts remove_char('eloquent')
---
+++ lesson_5/practice/6.rb
@@ -0,0 +1,6 @@
+[{ a: 'ant', b: 'elephant' }, { c: 'cat' }].select do |hash|
+  hash.all? do |key, value|
+    value[0] == key.to_s
+    puts key.to_s
+  end
+end
