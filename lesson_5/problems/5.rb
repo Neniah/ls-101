72fa489aa2dc599025ac3717248b2c8395d39c67
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-23 18:44:55 +0200

    total age

---
+++ lesson_5/problems/5.rb
@@ -0,0 +1,14 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+total = 0
+munsters.each_value do |data|
+  total += data['age'] if data['gender'] == 'male'
+end
+
+puts total
