3e5fcbabf01e583fb7049f995f4c60af5ec4db08
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-25 12:09:49 +0200

    rotate

---
+++ small-problems/medium-1/2.rb
@@ -0,0 +1,23 @@
+def rotate_array(arr)
+  result = []
+  arr.each do |number|
+    result << number
+  end
+  number = arr[0]
+  result.delete_at(0)
+  result << number
+  result
+end
+
+def rotate_rightmost_digits(number, n)
+  all_digits = number.to_s.chars
+  all_digits[-n..-1] = rotate_array(all_digits[-n..-1])
+  all_digits.join.to_i
+end
+
+puts rotate_rightmost_digits(735291, 1) == 735291
+puts rotate_rightmost_digits(735291, 2) == 735219
+puts rotate_rightmost_digits(735291, 3) == 735912
+puts rotate_rightmost_digits(735291, 4) == 732915
+puts rotate_rightmost_digits(735291, 5) == 752913
+puts rotate_rightmost_digits(735291, 6) == 352917
