570c9a09f1952f5f51c1c60afb09af137ea47df8
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-26 13:23:37 +0200

    friday ls

--- small-problems/medium-1/5.rb
+++ small-problems/medium-1/5.rb
@@ -7,7 +7,20 @@ def diamond(size)
   end
 end
 
+
+def diamondy(size)
+  puts ' ' * (size+1) + '*'
+  1.step(size, 2) do |x|
+    puts ' ' * (size-(x/2)) + '*' + ' ' * (x) + '*'
+  end
+  (size-2).step(1, -2) do |x|
+    puts ' ' * (size-(x/2)) + '*' + ' ' * (x) + '*'
+  end
+  puts ' ' * (size+1) + '*'
+end
 diamond(1)
 diamond(3)
 diamond(5)
 diamond(9)
+
+diamondy(5)
