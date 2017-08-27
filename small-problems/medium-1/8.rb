96f73805aa03d35b0926a0ed26fc0d69d07d4e9b
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-27 13:30:46 +0200

    fibonacci

---
+++ small-problems/medium-1/8.rb
@@ -0,0 +1,15 @@
+def fibonacci(number)
+  if number <= 2
+    return 1
+  elsif number > 2
+    fibonacci(number - 1) + fibonacci(number - 2)
+  end
+end
+
+puts fibonacci(1) == 1
+puts fibonacci(2) == 1
+puts fibonacci(3) == 2
+puts fibonacci(4) == 3
+puts fibonacci(5) == 5
+puts fibonacci(12) == 144
+puts fibonacci(20) == 6765
