10bee4bcfb05197a90ac39282bf4a0c16ed8e0f0
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-29 11:45:48 +0200

    tri-angles

---
+++ small-problems/medium-2/6.rb
@@ -0,0 +1,23 @@
+def triangle(a, b, c)
+  angles = [a, b, c]
+
+  case
+  when angles.reduce(:+) != 180, angles.include?(0)
+    :invalid
+  when angles.include?(90)
+    :right
+  when angles.all? { |angle| angle < 90 }
+    :acute
+  else
+    :obtuse
+  end
+end
+
+
+
+
+puts triangle(60, 70, 50) == :acute
+puts triangle(30, 90, 60) == :right
+puts triangle(120, 50, 10) == :obtuse
+puts triangle(0, 90, 90) == :invalid
+puts triangle(50, 50, 50) == :invalid
