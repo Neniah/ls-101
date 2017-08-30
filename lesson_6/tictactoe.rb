a9f4a7a175ba30182905e5c8f187317f0aac88f8
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-30 10:38:54 +0200

    X or 0 on board

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -1,13 +1,17 @@
 def display_board(brd)
   puts ""
   puts "    |    |    "
+  puts " #{brd[1]}   |  #{brd[2]}  | #{brd[3]}   "
   puts "    |    |    "
   puts "----+----+-----"
   puts "    |    |    "
+  puts " #{brd[4]}   |  #{brd[5]}  | #{brd[6]}   "
   puts "    |    |    "
   puts "----+----+-----"
   puts "    |    |    "
+  puts " #{brd[7]}   |  #{brd[8]}  | #{brd[9]}   "
   puts "    |    |    "
+
 end
 
 def initialize_board
@@ -15,3 +19,14 @@ def initialize_board
   (1..9).each {|num| new_board[num] = ' '}
   new_board
 end
+
+def player_places_piece(brd)
+  puts "Choose a square (1-9):"
+  square = gets.chomp.to_i
+  brd[square] = 'X'
+end
+
+board = initialize_board
+display_board(board)
+
+player_places_piece(board)
