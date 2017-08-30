6433436f4c35b365592c268c2d67af380705e681
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-08-30 13:15:13 +0200

    using pry

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -1,32 +1,52 @@
+require 'pry'
+
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKET = 'O'
+
 def display_board(brd)
   puts ""
   puts "    |    |    "
-  puts " #{brd[1]}   |  #{brd[2]}  | #{brd[3]}   "
+  puts " #{brd[1]}  | #{brd[2]}  | #{brd[3]}   "
   puts "    |    |    "
   puts "----+----+-----"
   puts "    |    |    "
-  puts " #{brd[4]}   |  #{brd[5]}  | #{brd[6]}   "
+  puts " #{brd[4]}  | #{brd[5]}  | #{brd[6]}   "
   puts "    |    |    "
   puts "----+----+-----"
   puts "    |    |    "
-  puts " #{brd[7]}   |  #{brd[8]}  | #{brd[9]}   "
+  puts " #{brd[7]}  | #{brd[8]}  | #{brd[9]}   "
   puts "    |    |    "
 
 end
 
 def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = ' '}
+  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
   new_board
 end
 
-def player_places_piece(brd)
-  puts "Choose a square (1-9):"
-  square = gets.chomp.to_i
-  brd[square] = 'X'
+def empty_squares(brd)
+  #binding.pry
+  brd.keys.select {|number| brd[number] == INITIAL_MARKER}
+end
+
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    puts "Choose a square (#{empty_squares(brd).join(',')}):"
+    square = gets.chomp.to_i
+
+    break if empty_squares(brd).include?(square)
+    puts "Sorry, that's not a valid choise."
+  end
+
+  brd[square] = PLAYER_MARKER
 end
 
 board = initialize_board
 display_board(board)
 
-player_places_piece(board)
+player_places_piece!(board)
+puts board.inspect
+display_board(board)
